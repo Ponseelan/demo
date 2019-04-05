72922ea648a53c87e7d481cf0276b6c9b9f15229
Ponseelan <ponseelan.k@gmail.com>
2019-04-06 00:38:25 +0530

    div issue is sorted

--- client/src/Register/app.RegisterComponent.ts
+++ client/src/Register/app.RegisterComponent.ts
@@ -1,17 +1,32 @@
-import { Component, AfterViewChecked } from '@angular/core';
+import { Component, AfterViewChecked, InjectionToken } from '@angular/core';
 import {User} from '../Model/user';
 import {Router} from '@angular/router';
 import { RegisterService } from 'src/service/RegisterService';
+import {HttpClient } from '@angular/common/http';
+ function getRegisterProvider(httpclient:HttpClient):RegisterService
+{
+   return new RegisterService(httpclient);
+}
+const  REGISTER_SERVICE_TOKEN=new InjectionToken<RegisterService>("REGISTER_SERVICE_TOKEN");
 @Component(
     {
         selector:'login',
-        templateUrl:"./app.RegisterComponent.html"
+        templateUrl:"./app.RegisterComponent.html",
+        providers:[{provide:REGISTER_SERVICE_TOKEN,deps:[HttpClient],useFactory:getRegisterProvider}]
     })
     export class RegisterComponent 
-    {
+    {        
         constructor(private registerService:RegisterService,private router:Router)
         {
             
+        }
+        ResetAlreadyLoginNameExistErrorMessage()
+        {
+            console.log("error");
+            if(!this.tohideErrorMessage)
+            {
+                this.tohideErrorMessage=true;
+            }
         }
         userModel=new User("","","","");
         tohideErrorMessage:Boolean=true;
