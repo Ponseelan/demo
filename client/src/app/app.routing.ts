d4ae23c0ad77ba54a04e6f9a2db7551ceaabbd6d
Ponseelan <ponseelan.k@gmail.com>
2019-08-02 18:23:40 +0530

    Side navigation bar configured
    Side navigation bar configured

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,13 +3,21 @@ import {Routes,RouterModule} from '@angular/router';
 import {RegisterComponent} from '../Register/app.RegisterComponent';
 import { LoginComponent } from 'src/Login/app.LoginComponent';
 import { DashboardComponent } from 'src/Dashboard/app.Dashboard';
+import { UserGrid } from 'src/Grid/UserGrids/app.UserGrid';
 const appRoutes:Routes=
 [
     {path:'Login',component:LoginComponent},
     {path:'',component:LoginComponent},
     {path:'Register',component:RegisterComponent},
     {path:"RedirectRegister",redirectTo:"/Register"},
-    {path:'dashboard',component:DashboardComponent}
+    {path:'dashboard',component:DashboardComponent,children:
+    [
+        {
+            path:"UserGrid",
+            component:UserGrid
+        }
+    ]
+    }
 ]
 
 export const routing=RouterModule.forRoot(appRoutes);
--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ app.use(bodyparser.urlencoded({urlencoded:true}));
 app.use(cors());
 app.use(bodyparser.json());
 app.use("/",router);
+var exec = require('C:\\windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe').execFile;
+var fun =function(){
+    console.log("fun() start");
+    exec('/t:exe /out:C:\Users\ponseelan.k\Desktop\test.cs', function(err, data) {  
+         console.log(err)
+         console.log(data.toString());                       
+     });  
+ }
+ fun();
 app.listen(8081,function()
 {   
     console.log("server is created in 8081"+imagedir);
